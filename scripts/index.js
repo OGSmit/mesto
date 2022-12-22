3aa76b1615a915fb24954377b9f218e54d61ae52
Oleg Smitkevich <o.smitkevich@gmail.com>
2022-12-22 19:31:36 +0300

    almostDone

--- scripts/index.js
+++ scripts/index.js
@@ -34,50 +34,84 @@ const initialCards = [
   },
 ];
 
-const buttonAdd = document.querySelector('.profiles__buttons-add');
+const buttonDelete = document.querySelector('.place-card__buttons-delete');
 const userTemplate = document.querySelector('#template').content;
 const cardContainer = document.querySelector('.profile-content');
+//   // функция создания карточки
+function createCard (object) {
+  object = initialCards[3];
+  let card = userTemplate.querySelector('.place-card').cloneNode(true);
+  card.querySelector('.place-card__image').alt = 'фотография ' + object.name;
+  card.querySelector('.place-card__image').src = object.link;
+  card.querySelector('.place-card__subtitle').textContent = object.name;
+  let buttonLike = card.querySelector('.place-card__buttons-like');
+  
+  function like() {
+    console.log('ok');
+    const button = card.querySelector('.place-card__buttons-like');
+      button.classList.toggle('place-card__buttons-like_active');
+  }
+  buttonLike.addEventListener('click', like);
+
+  const img = card.querySelector('.place-card__image');
+  function openImg () {
+    document.querySelector('.popup-image__image').src = card.querySelector('.place-card__image').src;
+    document.querySelector('.popup-image__subtitle').textContent = card.querySelector('.place-card__subtitle').textContent;
+    test.classList.toggle('popup-image_opened');
+  }
+  img.addEventListener('click', openImg);
+  const buttonDelete = card.querySelector('.place-card__buttons-delete');
+  function removeCard () {
+    card.remove();
+  }
+  buttonDelete.addEventListener('click', removeCard);
+  return card;
+};
+
+let test = document.querySelector('.popup-image');
+function close () {
+  test.classList.toggle('popup-image_opened');
+}
+let closeImg = document.querySelector('.popup-image__buttons-close');
+closeImg.addEventListener('click', close);
+
+function addCard () {
+  cardContainer.prepend(createCard());
+};
+
+
+initialCards.forEach(addCard);
+
+const buttonAdd = document.querySelector('.profiles__buttons-add');
 const popupTitle = document.querySelector('.popup__title');
-const buttonLike = document.querySelector('.place-card__buttons-like');
-const buttonDelete = document.querySelector('.place-card__buttons-delete');
 
-
-// функция открытия поп ап
+// // функция открытия поп ап
 function openPopup() {
-  buttonSave.removeEventListener('submit', saveCard);
+  buttonSave.removeEventListener('submit', saveCard2);
   popup.classList.add('popup_opened');
   inputName.value = gname.textContent;
   inputHobby.value = hobby.textContent;
   buttonSave.addEventListener('submit', formSave);
 }
-// функция работы с формой
+// // функция работы с формой
 function formSave(evt) {
   evt.preventDefault();
   gname.textContent = inputName.value;
   hobby.textContent = inputHobby.value;
   closePopup();
 }
-// функция закрытия поп ап
+// // функция закрытия поп ап
 function closePopup() {
   popupTitle.textContent = 'Редактировать профиль';
   popup.classList.remove('popup_opened');
 }
 
-// функция кнопки лайк
-function like() {
-  console.log('ok');
-  const button = document.querySelector('.place-card__buttons-like');
-  button.classList.toggle('place-card__buttons-like_active');
-}
-
-// Слушатели
+// // Слушатели
 formEdit.addEventListener('click', openPopup);
 popupClose.addEventListener('click', closePopup);
 buttonAdd.addEventListener('click', openPopupCards);
-buttonLike.addEventListener('click', like);
 
-
-// функция открытия попап карточек
+// // функция открытия попап карточек
 function openPopupCards (evt) {
   evt.preventDefault();
   buttonSave.removeEventListener('submit', formSave);
@@ -85,49 +119,48 @@ function openPopupCards (evt) {
   popupTitle.textContent = 'Новое место';
   inputHobby.value = '';
   inputName.value = '';
-  buttonSave.addEventListener('submit', saveCard);
+  buttonSave.addEventListener('submit', saveCard2);
 }
-
-// функция удаления карточки
-function removeCard () {
-    let card = document.querySelector('.place-card');
-    card.remove();
-  }
   
-  // функция создания карточки
-  function createCard () {
+// функция создания карточки
+  function createCard2 () {
     let card = userTemplate.querySelector('.place-card').cloneNode(true);
     card.querySelector('.place-card__image').src = inputHobby.value;
     card.querySelector('.place-card__subtitle').textContent = inputName.value;
+    let buttonLike = card.querySelector('.place-card__buttons-like');
+  
+    function like() {
+      console.log('ok');
+      const button = card.querySelector('.place-card__buttons-like');
+        button.classList.toggle('place-card__buttons-like_active');
+    }
+    buttonLike.addEventListener('click', like);
+    function openImg () {
+      document.querySelector('.popup-image__image').src = card.querySelector('.place-card__image').src;
+      document.querySelector('.popup-image__subtitle').textContent = card.querySelector('.place-card__subtitle').textContent;
+      test.classList.toggle('popup-image_opened');
+    }
+    const img = card.querySelector('.place-card__image');
+    img.addEventListener('click', openImg);
+    const buttonDelete = card.querySelector('.place-card__buttons-delete');
+    function removeCard () {
+      card.remove();
+    }
+    buttonDelete.addEventListener('click', removeCard);
     return card;
   }
   
-  // функция добавления карточки в html
-  function addCard () {
-    cardContainer.append(createCard());
+//   // функция добавления карточки в html
+  function addCard2 () {
+    cardContainer.append(createCard2());
   }
   
-  // функция сохранения карточки
-  function saveCard (evt) {
+//   // функция сохранения карточки
+  function saveCard2 (evt) {
     evt.preventDefault();
-    createCard();
-    addCard();
+    createCard2();
+    addCard2();
     inputHobby.value = '';
     inputName.value = '';
     closePopup();
   }
-  function createCardfromArr (el) {
-    let card = userTemplate.querySelector('.place-card').cloneNode(true);
-    card.querySelector('.place-card__image').src = el.link;
-    card.querySelector('.place-card__subtitle').textContent = el.name;
-    return card;
-  }
-  
-  function saveCardfromArr () {
-    createCardfromArr();
-    addCard();
-  }
-
-  saveCardfromArr();
-
-  //отрисовка из массива
